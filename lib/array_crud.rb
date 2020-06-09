12dfffcab85660cfe2cae7cf57371a98e734132f
Daniel Madera <dmadera0@gmail.com>
2020-06-09 14:43:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = [9,8,"am",6,5,4,3,2,1,0]
+  index_number = array[2]
 end
 
 def retrieve_first_element_from_array(array)
